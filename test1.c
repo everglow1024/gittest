d0af0f9161a39939dcad5664c5faa30b526a9f2b
everglow1024 <1765500161@qq.com>
2024-03-17 17:48:20 +0800

    Initial commit with test files

---
+++ test1.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# include <unistd.h>
+
+int main(void)
+{
+	while(1)
+	{
+
+		printf("circle ");
+		
+		sleep(10);
+	}
+	
+	printf("Hello World!! ");
+
+	return 0;	
+}
---
+++ test3.c
@@ -0,0 +1,10 @@
+# include<stdio.h>
+
+int a = 1;
+int main (void)
+
+{ 
+	printf("This is a test %d ", a );
+
+}
+
